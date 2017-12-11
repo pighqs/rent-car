3025723bbe7ed6045d3a7e9ce1269eabf96d271e
pighqs <peggy.hqs@gmail.com>
2017-12-11 14:31:19 +0100

    add sendpicture

--- server.js
+++ server.js
@@ -121,8 +121,19 @@ app.get("/findcars", function(req, res) {
 // })
 
 app.post('/sendpicture', function(req, res) {
-  console.log("dans send picture ");
-  console.log(req.files);
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let imageVoiture = req.files.imgCar;
+
+  // Use the mv() method to place the file somewhere on your server
+  imageVoiture.mv('./public/img/'+req.body.name+'.jpg', function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
 });
 
 var port = process.env["PORT"] || 8080;
